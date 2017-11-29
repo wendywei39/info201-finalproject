d016ab63c1788e4577436f8f66f0f194abe4406a
Jack Cole <h0tsh0t@uw.edu>
2017-11-28 18:53:09 -0800

    created api-keys file

--- Data.R
+++ Data.R
@@ -1,8 +1,10 @@
+source("api-keys.R")
+
 library(jsonlite)
 library(dplyr)
 library(httr)
 
-response <- GET("http://api.airvisual.com/v2/countries?key=zjh4dyiPncitNtYfF")
+response <- GET(paste0("http://api.airvisual.com/v2/countries?key=", api.key.air))
 
 body <- content(response, "text")
 parsed.data <- fromJSON(body)
